691e8218efcb38653fd884708700690578604042
DiegoAD10 <d.alarcond@duocuc.cl>
2026-09-26 16:26:46 -0300

    se modificaron las rutas en routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,35 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import Carrito from "./pages/Carrito/Carrito";
 import InicioSesion from "./pages/InicioSesion/InicioSesion";
-import Index from './pages/Index/index';
+import Index from "./pages/Index/Index.jsx";
 import Producto from "./pages/Producto/Producto";
 import Productos from "./pages/Productos/Productos";
 import RegistroUsuario from "./pages/RegistroUsuarios/RegistroUsuario";
 
 export const routes = createBrowserRouter([
-    {
-        path:'./pages/Carrito/Carrito',
-        element: <Carrito></Carrito>
-    },
-    {
-        path:'./pages/Index/index',
-        element: <Index></Index>
-    },
-    {
-        path:'./pages/InicioSesion/InicioSesion',
-        element: <InicioSesion></InicioSesion>
-    },
-    {
-        path:'./pages/Producto/Producto',
-        element: <Producto></Producto>
-    },
-    {
-        path:'./pages/Productos/Productos',
-        element: <Productos></Productos>
-    },
-    {
-        path:'./pages/RegistroUsuarios/RegistroUsuario',
-        element: <RegistroUsuario></RegistroUsuario>
-    }
-    
+  {
+    path: "/",                        
+    element: <Index />,
+  },
+  {
+    path: "/carrito",
+    element: <Carrito />,
+  },
+  {
+    path: "/inicio-sesion",            
+    element: <InicioSesion />,
+  },
+  {
+    path: "/producto",
+    element: <Producto />,
+  },
+  {
+    path: "/productos",
+    element: <Productos />,
+  },
+  {
+    path: "/registro",
+    element: <RegistroUsuario />,
+  },
 ]);
