f39aec557feaa620a5b85e11a5832560b517c736
DiegoAD10 <d.alarcond@duocuc.cl>
2026-09-26 17:21:22 -0300

    se modificó el index.css para que sea el estilo de base en el main y se puso despues de las importaciones de estilo de materialize para que lo puede sobreescribir

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './routes.jsx';
 // import './index.css'
-import './pages/Index/index.css'
+
 import App from './App.jsx'
 import RegistroUsuario from './pages/RegistroUsuarios/RegistroUsuario.jsx'
 import Productos from './pages/Productos/Productos.jsx'
@@ -12,6 +12,7 @@ import InicioSesion from './pages/InicioSesion/InicioSesion.jsx'
 import Carrito from './pages/Carrito/Carrito.jsx'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js';
+import './pages/Index/index.css'
 
 // Deje comentados los elementos ya creados para ir probandolos uno a uno; dejando siempre 1 y solo 1 descomentado
 // Pueden borrar y dejar solo el inicial cuando estimen conveniente
